20549092e86799844f1d8fbdb6e6f68a44962c25
Jesse Nauman <jnauman619@gmail.com>
2021-09-20 17:50:27 -0500

    Added content to about me section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -30,7 +30,7 @@ export const Home = () => {
         <div>
             <Container className="text-white">
                 <Row>
-                    <Col lg={8} className="mx-auto mt-5">
+                    <Col lg={6} className="mx-auto mt-5">
                         <Image src={self} className={classes.selfClass} roundedCircle />
                         <h1 className="text-center mt-4">Jesse Nauman</h1>
                         <h2 className="text-center">Full Stack Web Developer</h2>
@@ -42,7 +42,24 @@ export const Home = () => {
                                 <LinkedInIcon fontSize="large" className="icons text-center ms-4" />
                             </a>
                         </div>
-                <p className="ms-3" id="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <div className="ms-3" id="about-me">
+                            <p>
+                                I’m a perpetual learner and a problem solver. I enjoy technology and have a passion for the medical field.
+                                The dynamics of biology and medicine have always interested me but being an engineer provides me a sense of
+                                entertainment in the constant change. I started off building in JavaScript and have a drive for creativity
+                                in design layout and user experience. I’ve also had a lot of fun tackling the back end in Python and Flask.
+                            </p>
+                            <p>
+                                Outside of my studies and professional growth, my hobbies include gaming, fitness, and walking my dog.
+                            </p>
+                            <p>Skills:</p>
+                            <ul>
+                                <li>Languages: Python, JavaScript/TypeScript, JSON, HTML, CSS</li>
+                                <li>Frameworks/Libraries: Flask, React, Redux, Tailwind CSS, Bootstrap, Material UI</li>
+                                <li>Data: PostgreSQL, Pandas, NumPy, Pandas, Matplotlib, PySpark, Scikit-learn</li>
+                                <li>Tools: Git, GitHub, Insomnia, Google Firebase, Heroku, Slack, VSCode, Google Workspace, Microsoft Office</li>
+                            </ul>
+                        </div>
                     </Col>
                 </Row>
             </Container >
