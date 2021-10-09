d05f95a7c772f89b84fbdbf1e53fea1545ef619c
Jesse Nauman <jnauman619@gmail.com>
2021-10-09 11:39:12 -0500

    Fix link going outside LinkedIn icon

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -33,16 +33,44 @@ export const Home = () => {
                     <Col lg={6} className="mx-auto mt-5">
                         <Image src={self} className={classes.selfClass} roundedCircle />
                         <h1 className="text-center mt-4">Jesse Nauman</h1>
-                        <h2 className="text-center">Full Stack Web Developer</h2>
-                        <div className="icon-container text-center mt-3 mb-5">
-                            <a href="https://github.com/JesseN619" target="_blank">
-                                <GitHubIcon fontSize="large" className="icons text-center" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/jesse-nauman-70a39b18b/" target="_blank">
-                                <LinkedInIcon fontSize="large" className="icons text-center ms-4" />
-                            </a>
+                        <h2 className="text-center">Software Engineer</h2>
+                        <div className="icon-container text-center mt-3">
+                            <div>
+                                <a href="https://github.com/JesseN619" target="_blank">
+                                    <GitHubIcon fontSize="large" className="icons text-center" />
+                                </a>
+                            </div>
+                            <div className="ms-4">
+                                <a href="https://www.linkedin.com/in/jesse-nauman-70a39b18b/" target="_blank">
+                                    <LinkedInIcon fontSize="large" className="icons text-center" />
+                                </a>
+                            </div>
                         </div>
                         <div className="ms-3" id="about-me">
+                            <table className="mb-5">
+                                <tr>
+                                    <th className="skills-head">Languages</th>
+                                    <td className="skills">Python, JavaScript/TypeScript, JSON, HTML, CSS</td>
+                                </tr>
+                                <tr>
+                                    <th className="skills-head">Frameworks/Libraries</th>
+                                    <td className="skills">Flask, React, Redux, Tailwind CSS, Bootstrap, Material UI</td>
+                                </tr>
+                                <tr>
+                                    <th className="skills-head">Data</th>
+                                    <td className="skills">PostgreSQL, Pandas, NumPy, Pandas, Matplotlib, PySpark, Scikit-learn</td>
+                                </tr>
+                                <tr>
+                                    <th className="skills-head">Tools</th>
+                                    <td className="skills">Git, GitHub, Insomnia, Google Firebase, Heroku, Slack, VSCode, Google Workspace, Microsoft Office</td>
+                                </tr>
+                            </table>
+                            {/* <ul className="skills-list">
+                                <li><span className="skills-titles">Languages</span> Python, JavaScript/TypeScript, JSON, HTML, CSS</li>
+                                <li><span className="skills-titles">Frameworks</span>/Libraries Flask, React, Redux, Tailwind CSS, Bootstrap, Material UI</li>
+                                <li><span className="skills-titles">Data</span> PostgreSQL, Pandas, NumPy, Pandas, Matplotlib, PySpark, Scikit-learn</li>
+                                <li><span className="skills-titles">Tools</span> Git, GitHub, Insomnia, Google Firebase, Heroku, Slack, VSCode, Google Workspace, Microsoft Office</li>
+                            </ul> */}
                             <p>
                                 I’m a perpetual learner and a problem solver. I enjoy technology and have a passion for the medical field.
                                 The dynamics of biology and medicine have always interested me but being an engineer provides me a sense of
@@ -52,13 +80,6 @@ export const Home = () => {
                             <p>
                                 Outside of my studies and professional growth, my hobbies include gaming, fitness, and walking my dog.
                             </p>
-                            <p>Skills:</p>
-                            <ul>
-                                <li>Languages: Python, JavaScript/TypeScript, JSON, HTML, CSS</li>
-                                <li>Frameworks/Libraries: Flask, React, Redux, Tailwind CSS, Bootstrap, Material UI</li>
-                                <li>Data: PostgreSQL, Pandas, NumPy, Pandas, Matplotlib, PySpark, Scikit-learn</li>
-                                <li>Tools: Git, GitHub, Insomnia, Google Firebase, Heroku, Slack, VSCode, Google Workspace, Microsoft Office</li>
-                            </ul>
                         </div>
                     </Col>
                 </Row>
