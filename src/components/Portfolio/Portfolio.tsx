6d0e35e64933cb080f3bcc555454ba6aef76cc80
Jesse Nauman <jnauman619@gmail.com>
2021-10-11 19:04:18 -0500

    Add ProducerFinder to portfolio page. Move card height and weight to styles file.

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import consolesImg from '../../assets/images/consoles-img.png';
 import calculator from '../../assets/images/calculator.png';
-import blackjack from '../../assets/images/blackjack.png';
+import producerFinder from '../../assets/images/producerfinder.png';
 
 export const Portfolio = () => {
     return (
@@ -13,7 +13,27 @@ export const Portfolio = () => {
             <Container>
                 <Row className="mx-auto">
                     <Col lg={4} md={6} className="mt-5 mx-auto">
-                        <Card className="mx-auto border border-primary border-4" bg="primary" style={{ width: '18rem', height: '21rem' }}>
+                        <Card className="mx-auto border border-primary border-4 portfolio-card" bg="primary">
+                            <a href="https://producerfinder-6aca0.web.app/" target="_blank">
+                                <Card.Img variant="top" src={producerFinder} />
+                            </a>
+                            <Card.Body className="d-flex flex-column justify-content-between">
+                                <Card.Title>ProducerFinder</Card.Title>
+                                <Card.Text>
+                                    Enter a song and find the producer and their most popular songs. 
+                                    Add their songs to your Spotify playlists. 
+                                    Built with TypeScript, React, Redux, TailwindCSS.
+                                </Card.Text>
+                                <div>
+                                    <Card.Link className="link-light" href="https://github.com/JesseN619/ProducerFinder" target="_blank">GitHub</Card.Link>
+                                    <Card.Link className="link-light" href="https://producerfinder-6aca0.web.app/" target="_blank">App</Card.Link>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col lg={4} md={6} className="mt-5 mx-auto">
+                        <Card className="mx-auto border border-primary border-4 portfolio-card" bg="primary">
                             <a href="https://github.com/JesseN619/game_console_react" target="_blank">
                                 <Card.Img variant="top" src={consolesImg} />
                             </a>
@@ -32,7 +52,7 @@ export const Portfolio = () => {
                     </Col>
 
                     <Col lg={4} md={6} className="mt-5 mx-auto">
-                        <Card className="mx-auto border border-primary border-4" bg="primary" style={{ width: '18rem', height: '21rem' }}>
+                        <Card className="mx-auto border border-primary border-4 portfolio-card" bg="primary">
                             <a href="https://jessen619.github.io/Calculator/" target="_blank">
                                 <Card.Img variant="top" src={calculator} />
                             </a>
@@ -44,24 +64,6 @@ export const Portfolio = () => {
                                 <div>
                                     <Card.Link className="link-light" href="https://github.com/JesseN619/Calculator" target="_blank">GitHub</Card.Link>
                                     <Card.Link className="link-light" href="https://jessen619.github.io/Calculator/" target="_blank">App</Card.Link>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col lg={4} md={6} className="mt-5 mx-auto">
-                        <Card className="mx-auto border border-primary border-4" bg="primary" style={{ width: '18rem', height: '21rem' }}>
-                            <a href="https://github.com/JesseN619/blackjack" target="_blank">
-                                <Card.Img variant="top" src={blackjack} />
-                            </a>
-                            <Card.Body className="d-flex flex-column justify-content-between">
-                                <Card.Title>Python Blackjack</Card.Title>
-                                <Card.Text>
-                                    Blackjack game in the terminal created with Python.
-                                </Card.Text>
-                                <div>
-                                    <Card.Link className="link-light" href="https://github.com/JesseN619/blackjack" target="_blank">GitHub</Card.Link>
-                                    {/* <Card.Link className="link-light" href="https://jessen619.github.io/Calculator/" target="_blank">App</Card.Link> */}
                                 </div>
                             </Card.Body>
                         </Card>
